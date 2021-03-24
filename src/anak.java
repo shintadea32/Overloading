a7d26614583645aa4e873c2f43cc70f9d90a2889
shintadewi29rpl <shinta_dewi_29rpl@student.smktelkom-mlg.sch.id>
2021-03-24 21:26:27 +0700

    overloading

---
+++ src/anak.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author I d e a P a d
+ */
+public class anak {
+
+    public void luas(String l){
+        System.out.println("Macam Macam Luas Bangun Datar");
+    }
+    public void luaspersegi(int s1, int s2){
+        System.out.println("Luas Persegi : "+(s1+s2));
+    }
+    public void luaspersegipanjang (int p, int l){
+        System.out.println("Luas Persegi Panjang : "+(p*l));
+    }
+    public void luassegitiga (int a,int b){
+        System.out.println("Luas Segitiga :"+(a*b/2));
+    }
+    public void luaslingkaran (double phi, int r){
+        System.out.println("Luas Lingkaran : "+(phi*r*r));
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author I d e a P a d
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        anak objek = new anak();
+        objek.luas ("");
+        objek.luaspersegi(10, 10);
+        objek.luaspersegipanjang(12, 15);
+        objek.luassegitiga(12,18);
+        objek.luaslingkaran(3,14*5*5);
+        
+        
+    }
+    
+}
