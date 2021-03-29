d1b81b5c1c5f6b21b6f2aa5125e21d86225288fb
shintadewi29rpl <72966161+shintadewi29rpl@users.noreply.github.com>
2021-03-29 07:58:48 +0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -14,12 +14,18 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //membuat objek untuk memanggil class anak
         anak objek = new anak();
+        //menggunakan objek untuk memanggil method luas
         objek.luas ("");
+        //menggunakan objek untuk memanggil method luaspersegi dan juga ditambahkan angka dari masing masing sisi
         objek.luaspersegi(10, 10);
+        //menggunakan objek untuk memanggil method luaspersegipanjang dan juga ditambahkan angka dari masing masing panjang dan lebar
         objek.luaspersegipanjang(12, 15);
+        //menggunakan objek untuk memanggil method luassegitiga dan juga ditambahkan angka dari masing masing sisi
         objek.luassegitiga(12,18);
+        //menggunakan objek untuk memanggil method luaslingkaran dan juga ditambahkan angka dari masing masing phi dan r
         objek.luaslingkaran(3,14*5*5);
         
         
